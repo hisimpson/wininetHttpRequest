cba85fa3d7efb6acb614f561ef3134ecbebd31fb
mysimpson <alldream78@daum.net>
2020-11-16 06:09:00 +0900

    refs #0 httpRequest 에러 12029 수정, HttpOpenRequest 옵션을 변경해서 해결

--- HttpRequest.cpp
+++ HttpRequest.cpp
@@ -1,34 +1,36 @@
-#include <Windows.h>
-#include <WinInet.h>
+#include <windows.h>
+#include <winInet.h>
 #include <stdio.h>
 
 #include <string>
 #include <atlbase.h>
 #include <atlconv.h>
 
-
 #pragma comment (lib, "Wininet.lib")
 #pragma comment (lib, "urlmon.lib")
 
 bool PostHttp()
 {
-    HINTERNET hInternet = InternetOpen("HTTPTEST", INTERNET_OPEN_TYPE_PRECONFIG, NULL, NULL, 0);
+    HINTERNET hInternet = InternetOpen(_T("HTTPTEST"), INTERNET_OPEN_TYPE_PRECONFIG, NULL, NULL, 0);
     if(hInternet == NULL)
         return false;;
 
-    HINTERNET hInternetConnect = InternetConnect(hInternet, "localhost", 8881, "", "", INTERNET_SERVICE_HTTP, 0, 0);
+    HINTERNET hInternetConnect = InternetConnect(hInternet, _T("localhost"), 8881, _T(""), _T(""), INTERNET_SERVICE_HTTP, 0, 0);
     if(hInternetConnect == NULL)
     {
         InternetCloseHandle(hInternetConnect);
         return false;
     }
 
-    HINTERNET hOpenRequest = HttpOpenRequest( hInternetConnect, "POST", "/login.jsp", HTTP_VERSION, "", 
+    HINTERNET hOpenRequest = HttpOpenRequest( hInternetConnect, _T("POST"), _T("/login.jsp"), HTTP_VERSION, _T(""), 
                                               NULL,
-                                              INTERNET_FLAG_RELOAD | INTERNET_FLAG_DONT_CACHE | INTERNET_FLAG_KEEP_CONNECTION |INTERNET_FLAG_PRAGMA_NOCACHE | INTERNET_FLAG_NO_CACHE_WRITE, // ¿Ã∞Õ µ 
-                                              0);
+                                             INTERNET_FLAG_RELOAD,  // Ïù¥Í≤É Îê®
+                                             //INTERNET_FLAG_RELOAD | INTERNET_FLAG_DONT_CACHE | INTERNET_FLAG_KEEP_CONNECTION |INTERNET_FLAG_PRAGMA_NOCACHE | INTERNET_FLAG_NO_CACHE_WRITE, // Ïù¥Í≤É Îê®
+                                             //INTERNET_FLAG_KEEP_CONNECTION | INTERNET_FLAG_PRAGMA_NOCACHE | INTERNET_FLAG_NO_CACHE_WRITE, //Ïù¥Í≤É HttpSendRequestÎê®
+                                             //INTERNET_FLAG_SECURE|INTERNET_FLAG_IGNORE_CERT_CN_INVALID|INTERNET_FLAG_IGNORE_CERT_DATE_INVALID, //Ïù¥Í≤ÉÏïàÎê®
+                                             0);
 
-    //¿Ã∫Œ∫–ø° internalSetoption¿ª ¡ÿ¥Ÿ, Flag∏¶ πŸ≤„ ∞°∏Èº≠ Ω««‡ «ÿ∫ª¥Ÿ. 
+    //Ïù¥Î∂ÄÎ∂ÑÏóê internalSetoptionÏùÑ Ï§ÄÎã§, FlagÎ•º Î∞îÍøî Í∞ÄÎ©¥ÏÑú Ïã§Ìñâ Ìï¥Î≥∏Îã§. 
 #if  1
     DWORD dwFlags = 0 ;
     dwFlags = SECURITY_FLAG_IGNORE_UNKNOWN_CA   |
@@ -41,29 +43,29 @@ bool PostHttp()
     InternetSetOption(hOpenRequest, INTERNET_OPTION_SECURITY_FLAGS, &dwFlags, sizeof(dwFlags));
 #endif
 
-    char szPostData[2048] = {0};
-    lstrcpy(szPostData, "user_name=µ—«œ≥™&user_id=hong&user_address=korea");
+    TCHAR szPostData[2048] = {0};
+    lstrcpy(szPostData, _T("user_name=ÎëòÌïòÎÇò&user_id=hong&user_address=korea"));
     std::string strPostData = CW2A(CT2W(szPostData), CP_UTF8);
-    int postDataLength = strPostData.length();
+    int postDataLength = (int)strPostData.length();
 
     // post header
     char szLen[MAX_PATH] = {0};
     char szHeader[2048] = {0};
 
-    wsprintf(szLen, "%d", postDataLength);
-    strcat(szHeader, "Accept: text/*\r\n");
-    strcat(szHeader, "User-Agent: Mozilla/4.0 (compatible; MSIE 5.0;* Windows NT)\r\n");
-    strcat(szHeader, "Content-type: application/x-www-form-urlencoded\r\n");
-    strcat(szHeader, "Content-length: ");
+    wsprintf(szLen, _T("%d"), postDataLength);
+    strcat(szHeader, _T("Accept: text/*\r\n"));
+    strcat(szHeader, _T("User-Agent: Mozilla/4.0 (compatible; MSIE 5.0;* Windows NT)\r\n"));
+    strcat(szHeader, _T("Content-type: application/x-www-form-urlencoded\r\n"));
+    strcat(szHeader, _T("Content-length: "));
     strcat(szHeader, szLen);
-    strcat(szHeader, "\r\n\n");
+    strcat(szHeader, _T("\r\n\n"));
 
 
     BOOL bHeaderRequest = HttpAddRequestHeaders(hOpenRequest, szHeader, -1L, HTTP_ADDREQ_FLAG_ADD);
     if(!bHeaderRequest)
     {
         DWORD errId = GetLastError();  //error code 87
-        printf("Fail HttpAddRequestHeaders %d \n", errId);
+		printf("Failed: Error in HttpAddRequestHeaders %d \n", errId);
     }
     else
     {
@@ -82,13 +84,13 @@ bool PostHttp()
                                            sizeof(szBuf),
                                            &dwSize);
 
-            //MessageBox(NULL, CW2T(CA2W(szBuf, CP_UTF8)), "post test", MB_OK);
-            printf(CW2T(CA2W(szBuf, CP_UTF8)));
+            //MessageBox(NULL, CW2T(CA2W(szBuf, CP_UTF8)), _T("post test"), MB_OK);
+            printf(CW2A(CA2W(szBuf, CP_UTF8)));
         }
         else
 	    {
-             DWORD errId = GetLastError();  //error code 12029
-             printf("Fail HttpSendRequest %d \n", errId);
+             DWORD errId = GetLastError();  //error code 12029, INTERNET_FLAG_SECURE ÏòµÏÖòÏù¥ Îì§Ïñ¥Í∞ÄÎ©¥ Î∞úÏÉù
+			 printf("Failed: Error in HttpSendRequest %d \n", errId);
 	    }
     }
     BOOL bRet = InternetCloseHandle(hOpenRequest);
